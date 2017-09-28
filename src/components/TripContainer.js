bb3e9bec85d337f619ec0914233f2b91ea2df630
bother7 <github email address>
2017-09-28 15:31:18 -0400

    kennys first decision

--- src/components/TripContainer.js
+++ src/components/TripContainer.js
@@ -6,6 +6,6 @@ export default class TripContainer extends React.Component {
 
 
   render() {
-    return (<div>screw you kenny </div>)
+    return (<div>screw you kenny <img src="/imgs/pikachu_phd.png" /></div>)
   }
 }
