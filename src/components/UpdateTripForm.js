5cfe53996e9496c0b19d71556d48927f8e43a2d0
bother7 <github email address>
2017-10-02 18:07:36 -0400

    edit started working

--- src/components/UpdateTripForm.js
+++ src/components/UpdateTripForm.js
@@ -6,39 +6,66 @@ class UpdateTripForm extends React.Component{
       return false
     } else if (this.props.locations.includes(" -- ")) {
       return this.setState({
-        locations: (this.props.locations.split(" -- "))
+        locations: (this.props.locations.split(" -- ")),
+        location_size: this.props.locations.split(" -- ").length
       })
     } else {
       return this.setState({
-        locations: [this.props.locations]
+        locations: [this.props.locations],
+        location_size: 1
       })
     }
   }
   state = {
     name: "",
-    locations: false
+    locations: false,
+    location_size: 0
   }
 
   locationInputs = () => {
     return this.state.locations.map((location, index) => {
-        return <input id={index} type="text" value={location} />
+      const locationInfo = location.split("||")
+        return (<ul><input id={`location_name_${index}`} type="text" value={locationInfo[0]} onChange={this.superHandler}/>
+      <input type="date" id={`start_date_${index}`}  name="start_date"  value={locationInfo[1]} onChange={this.superHandler}/>
+        <input type="date" name="end_date" id={`end_date_${index}`} value={locationInfo[2]} onChange={this.superHandler}/></ul>
+      )
     })
   }
 
-  // componentDidMount(){
-  //   fetch("http://localhost:3000/api/v1/user_trips")
-  // }
   componentDidMount(){
     this.newlocations()
   }
 
+  handleUpdate = (event) => {
+    event.preventDefault()
+    debugger
+  }
+
+  superHandler = (event) => {
+    console.log("this is fine", event.target.value, event.target.id)
+    const arr = this.state.locations.map((location, index) => {
+      const locName = `location_name_${index}`
+      const stDate = `start_date_${index}`
+      const endDate = `end_date_${index}`
+      return Object.assign( {},
+        {[locName]:location.split("||")[0],
+        [stDate]:location.split("||")[1],
+        [endDate]:location.split("||")[2]}
+      )
+    })
+    arr[0][event.target.id] = event.target.value
+    this.setState({locations: [Object.values(arr[0]).join("||")]})
+  }
+
   render() {
-    // console.log(this.props.locations.includes(" "));
     console.log(this.state.locations)
     return(
-      <form>
+      <form onSubmit={this.handleUpdate}>
         {this.state.locations ? this.locationInputs() : null}
-        <input type="submit" />
+        <br></br><input type="text" />
+        <input type="date" name="new_start_date" />
+        <input type="date" name="new_end_date" /><br></br>
+        <input type="submit" value="Update Trip" />
       </form>
     )
   }
