3a4c762328c8117373d3353729c0b015e52de6bf
linkcoding <kenny.lin.91@gmail.com>
2018-03-04 11:50:50 -0500

    changed trip form

--- src/components/TripForm.js
+++ src/components/TripForm.js
@@ -4,20 +4,20 @@ const TripForm = (props) => {
   return (
     <form className="formRight" onSubmit={props.handleTripSubmit}>
       <label>
-      Name of Your Trip:
+        Name of Your Trip:
       <input type="text" name="name" onChange={props.changeTripName} value={props.newTripName}/>
       </label>
       <label>
-      Location:
+        Location:
       <input type="text" name="name" onChange={props.changeLocation} value={props.location}/>
       </label>
       <label>
-      Start Date:
+        Start Date:
       <input type="date" name="start_date" onChange={props.changeTripStart}/>
 
       </label>
       <label>
-      End Date:
+        End Date:
       <input type="date" name="end_date" onChange={props.changeTripEnd}/>
       </label>
       <input className="fsSubmitButton" type="submit" value="Submit" />
